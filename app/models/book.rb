0740cd2e6312dcadc0ca212dfbb5de542b6e8b6f
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-10-16 10:30:15 +0900

    add method for kindle edition check

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,7 @@
 class Book < ApplicationRecord
   belongs_to :user
   validates :isbn_10, presence: true, length: { is: 10 }
-  validate :check_isbn_and_set_name
+  validate :check_isbn_and_set_title
 
   class << self
     def build(params, current_user)
@@ -15,8 +15,9 @@ class Book < ApplicationRecord
     "https://www.amazon.co.jp/dp/#{isbn_10}/"
   end
 
-  def check_isbn_and_set_name
+  def check_isbn_and_set_title
     return unless self.errors[:isbn_10].blank?
+    return unless isbn_10_changed?
 
     item = Amazon::Ecs.item_lookup(isbn_10, { ResponseGroup: 'ItemAttributes,AlternateVersions' })
     if item.error
@@ -24,10 +25,28 @@ class Book < ApplicationRecord
       return
     end
     self.title = item.get_element('ItemAttributes/Title').get
+  end
 
-    alternate_versions = res.get_element('AlternateVersions')
-    if alternate_versions
-      self.has_kindle_edition = true
+  def set_kindle_edition_info!
+    item = Amazon::Ecs.item_lookup(isbn_10, { ResponseGroup: 'ItemAttributes,AlternateVersions' })
+    if item.error
+      errors.add(:aws, item.error)
+      return
     end
+
+    alternate_versions = item.get_element('AlternateVersions')
+    return unless alternate_versions
+
+    self.has_kindle_edition = true
+
+    # NOTE For avoid 503 error.
+    sleep 2
+
+    asin = alternate_versions.get_element("AlternateVersion/ASIN").get
+    item = Amazon::Ecs.item_lookup(asin, { ResponseGroup: 'ItemAttributes,AlternateVersions' })
+    unless item.error
+      self.kindle_edition_release_date = item.get_element("ReleaseDate").get
+    end
+    save!
   end
 end

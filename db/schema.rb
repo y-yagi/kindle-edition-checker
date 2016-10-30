d4f44cef2d8c35a43ed85925f3d5b2480afaa1d5
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-10-31 08:05:58 +0900

    add `chrome_notification` to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161024061924) do
+ActiveRecord::Schema.define(version: 20161030061924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,12 +28,13 @@ ActiveRecord::Schema.define(version: 20161024061924) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "uid",                                null: false
-    t.string   "provider",                           null: false
+    t.string   "uid",                                 null: false
+    t.string   "provider",                            null: false
     t.string   "email"
-    t.datetime "created_at",                         null: false
-    t.datetime "updated_at",                         null: false
-    t.boolean  "email_notification", default: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.boolean  "email_notification",  default: false
+    t.boolean  "chrome_notification", default: false
   end
 
   add_foreign_key "books", "users"

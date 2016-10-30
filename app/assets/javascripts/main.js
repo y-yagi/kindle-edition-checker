64236599574eddad856f7ea0bb26e124202d92aa
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-10-31 08:22:51 +0900

    remove log

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,15 +1,13 @@
 if ('serviceWorker' in navigator) {
-  console.log('Service Worker is supported');
   navigator.serviceWorker.register('/assets/sw.js').then(function() {
     return navigator.serviceWorker.ready;
   }).then(function(reg) {
-    console.log('Service Worker is ready :^)', reg);
     reg.pushManager.subscribe({userVisibleOnly: true}).then(function(sub) {
       var subscriptionId = sub.endpoint.split("/").pop();
       console.log('endpoint:', sub.endpoint);
       console.log('subscriptionId:', subscriptionId);
     });
   }).catch(function(error) {
-    console.log('Service Worker error :^(', error);
+    console.log('Service Worker error :', error);
   });
 }

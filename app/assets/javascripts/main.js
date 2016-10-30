1a8bc154368310b485eafe72616d0a666c9dffe8
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-10-31 08:14:31 +0900

    organize js files

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   console.log('Service Worker is supported');
-  navigator.serviceWorker.register('sw.js').then(function() {
+  navigator.serviceWorker.register('/assets/sw.js').then(function() {
     return navigator.serviceWorker.ready;
   }).then(function(reg) {
     console.log('Service Worker is ready :^)', reg);

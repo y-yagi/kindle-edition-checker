374e34d43951d1720470d77843598217d459beee
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-10-31 08:50:55 +0900

    register chrome subscription id

--- app/assets/javascripts/settings.js
+++ app/assets/javascripts/settings.js
@@ -1,11 +1,11 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/assets/sw.js').then(function() {
+  navigator.serviceWorker.register('/sw.js').then(function() {
     return navigator.serviceWorker.ready;
   }).then(function(reg) {
     reg.pushManager.subscribe({userVisibleOnly: true}).then(function(sub) {
       var subscriptionId = sub.endpoint.split("/").pop();
-      console.log('endpoint:', sub.endpoint);
-      console.log('subscriptionId:', subscriptionId);
+      console.log(subscriptionId)
+      $("#user_chrome_subscription_id").val(subscriptionId);
     });
   }).catch(function(error) {
     console.log('Service Worker error :', error);

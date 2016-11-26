32d38610c3fc305d0c6cb9f6ad19e86491c1e41d
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-11-26 09:51:24 +0900

    organize tests

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -2,10 +2,11 @@ require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
   test "kindle_notification" do
-    mail = UserMailer.kindle_notification
-    assert_equal "Kindle notification", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    user = users(:google)
+    mail = UserMailer.kindle_notification(user, user.books.unnotified)
+    assert_equal "[Kindle Edition Checker]Kindle版の発売日をお知らせします", mail.subject
+    assert_equal [user.email], mail.to
+    assert_equal %w(noreply@kindle-edition-checker.herokuapp.com), mail.from
+    assert_match "下記書籍のKindle版がamazonに登録されたのでお知らせします。\r\n\r\n  スケッチブック 12 (BLADE COMICS)  発売日: 2016-10-17\r\n  https://www.amazon.co.jp/dp/4800006198/\r\n\r\n", mail.body.encoded
   end
 end

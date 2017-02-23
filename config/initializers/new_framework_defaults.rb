01655311702e2760a117282bf83c1e889e28fb64
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2017-02-24 07:39:42 +0900

    add new default

--- config/initializers/new_framework_defaults.rb
+++ config/initializers/new_framework_defaults.rb
@@ -19,3 +19,7 @@ Rails.application.config.active_record.belongs_to_required_by_default = true
 
 # Configure SSL options to enable HSTS with subdomains. Previous versions had false.
 Rails.application.config.ssl_options = { hsts: { subdomains: true } }
+
+# Unknown asset fallback will return the path passed in when the given
+# asset is not present in the asset pipeline.
+Rails.application.config.assets.unknown_asset_fallback = false

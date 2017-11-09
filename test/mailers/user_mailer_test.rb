0dc8ab163cffbb12f5fd86792d3caf92d6808577
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2017-11-10 08:26:44 +0900

    Use assert_equal

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -7,6 +7,6 @@ class UserMailerTest < ActionMailer::TestCase
     assert_equal "[Kindle Edition Checker]Kindle版の発売日をお知らせします", mail.subject
     assert_equal [user.email], mail.to
     assert_equal %w(noreply@kindle-edition-checker.herokuapp.com), mail.from
-    assert_match "下記書籍のKindle版がamazonに登録されたのでお知らせします。\r\n\r\n  スケッチブック 12 (BLADE COMICS)  発売日: 2016-10-17\r\n  https://www.amazon.co.jp/dp/4800006198/\r\n\r\n", mail.body.encoded
+    assert_equal "下記書籍のKindle版がamazonに登録されたのでお知らせします。\n\n  スケッチブック 12 (BLADE COMICS)  発売日: 2016-10-17\n  https://www.amazon.co.jp/dp/4800006198/\n\n", mail.body.to_s
   end
 end

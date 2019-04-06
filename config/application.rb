ae40de87a47efe5a546c0cbbbfad332bcf23d9d6
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2019-04-07 07:27:40 +0900

    Run app:update

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module KindleChecker
   class Application < Rails::Application
-    config.load_defaults "5.2"
+    config.load_defaults "6.0"
     config.generators do |g|
       g.assets false
       g.helper false
